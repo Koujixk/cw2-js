f323a5e6ade5cd4a76f604cd6f10e328d437a6e4
Koujixk <arsiliazov@gmail.com>
2022-08-08 23:31:51 +0600

    added sorting by title

--- script.js
+++ script.js
@@ -25,12 +25,11 @@ const addingValuesToTable = (arr) => {
     tableFetch.innerHTML = `
     <tr>
         <th>${Object.keys(arr[0])[0]}</th>
-        <th>${Object.keys(arr[0])[1]}</th>
+        <th><div id='tableHeaderYear' onclick='sortingArray(filmsArray)' class='table-header'><span>${Object.keys(arr[0])[1]}</span><div class='arrow arrow-down' id='arrow'></div></div></th>
         <th>${Object.keys(arr[0])[9]}</th>
         <th>${Object.keys(arr[0])[4]}</th>
     </tr>
     `
-    tableHeaders = document.querySelectorAll('th')
     arr.forEach((el, index) => {
 
         tableFetch.innerHTML += `
@@ -52,8 +51,8 @@ const refreshingTableUsingInput = (arr) => {
     tableFetch.innerHTML += `
     <tr>
         <th>${Object.keys(arr[0])[0]}</th>
-        <th>${Object.keys(arr[0])[1]}</th>
-        <th>${Object.keys(arr[0])[9]}</th>
+        <th><div id='tableHeaderYear' onclick='sortingArray(filmsArray)' class='table-header'><span>${Object.keys(arr[0])[1]}</span><div class='arrow'  id='arrow'></div></div></th>
+        <th>${Object.keys(arr[0])[9]}</div></th>
         <th>${Object.keys(arr[0])[4]}</th>
     </tr>
     `
@@ -85,16 +84,14 @@ const refreshingTableUsingInput = (arr) => {
 
 const reset = (arr) => {
     searchInput.value = ''
-    tableFetch.innerHTML = ''
     tableFetch.innerHTML = `
     <tr>
         <th>${Object.keys(arr[0])[0]}</th>
-        <th>${Object.keys(arr[0])[1]}</th>
+        <th><div id='tableHeaderYear' onclick='sortingArray(filmsArray)' class='table-header'><span>${Object.keys(arr[0])[1]}</span><div class='arrow' id='arrow'></div></div></th>
         <th>${Object.keys(arr[0])[9]}</th>
         <th>${Object.keys(arr[0])[4]}</th>
     </tr>
     `
-    tableHeaders = document.querySelectorAll('th')
     arr.forEach((el, index) => {
 
         tableFetch.innerHTML += `
@@ -107,4 +104,75 @@ const reset = (arr) => {
         `
         document.getElementById(`${"table-image" + index}`).style.backgroundImage = `url(${el.image})`
     })
+}
+let isClicked = false
+const sortingArray = (arr) => {
+    if(isClicked == false){
+        tableFetch.innerHTML =''
+        tableFetch.innerHTML = `
+        <tr>
+            <th>${Object.keys(arr[0])[0]}</th>
+            <th><div id='tableHeaderYear' onclick='sortingArray(filmsArray)'><span>${Object.keys(arr[0])[1]}</span><div class='arrow arrow-up'></div></div></th>
+            <th>${Object.keys(arr[0])[9]}</th>
+            <th>${Object.keys(arr[0])[4]}</th>
+        </tr>
+        `
+        let sortedFilms = arr.sort((a, b) => {
+            if (a.title > b.title) {
+                return 1;
+              }
+              if (a.title < b.title) {
+                return -1;
+              }
+              return 0;
+        })
+        sortedFilms.forEach((el, index) => {
+    
+            tableFetch.innerHTML += `
+            <tr>
+                <td>${el.id}</td>
+                <td>${el.title}</td>
+                <td>${el.release_date}</td>
+                <td><div id="${"table-image" + index}" class='table-images'></div></td>
+            </tr>
+            `
+            document.getElementById(`${"table-image" + index}`).style.backgroundImage = `url(${el.image})`
+        })
+        isClicked = true
+        console.log(isClicked)
+    }else if(isClicked == true){
+        tableFetch.innerHTML = ''
+        tableFetch.innerHTML = `
+        <tr>
+            <th>${Object.keys(arr[0])[0]}</th>
+            <th><div id='tableHeaderYear' onclick='sortingArray(filmsArray)' class='table-header'><span>${Object.keys(arr[0])[1]}</span><div class='arrow arrow-down' id='arrow'></div></div></th>
+            <th>${Object.keys(arr[0])[9]}</th>
+            <th>${Object.keys(arr[0])[4]}</th>
+        </tr>
+        `
+        let sortedFilms = arr.sort((a, b) => {
+            if (a.title > b.title) {
+                return -1;
+              }
+              if (a.title < b.title) {
+                return 1;
+              }
+              return 0;
+        })
+        sortedFilms.forEach((el, index) => {
+    
+            tableFetch.innerHTML += `
+            <tr>
+                <td>${el.id}</td>
+                <td>${el.title}</td>
+                <td>${el.release_date}</td>
+                <td><div id="${"table-image" + index}" class='table-images'></div></td>
+            </tr>
+            `
+            document.getElementById(`${"table-image" + index}`).style.backgroundImage = `url(${el.image})`
+        })
+        isClicked = false
+        console.log('works', isClicked)
+    }
+    
 }
